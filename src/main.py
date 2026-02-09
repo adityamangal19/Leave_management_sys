63bf38dba41b014c55279c0d2fc82a8d2b58e83e
adityamangal19 <aditya.mangal@gammaedge.io>
2026-02-09 16:09:05 +0530

    Add comment for FastAPI app initialization
    Added a comment to clarify the purpose of the FastAPI app.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter
 from src.database import Base, engine
 from src.routes import auth,leaves
 from src import models
-
+## To Check and learn about Get fetch command
 app=FastAPI()
 
 @app.on_event("startup")  # Connecting db before import
